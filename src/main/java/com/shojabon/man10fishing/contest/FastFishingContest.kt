fe2d1b7f64863e270572dea814c6d41317b8ace7
JToTl <80580108+JToTl@users.noreply.github.com>
2023-04-11 01:16:12 +0900

    fastfishingのおおまかな処理追加

--- src/main/java/com/shojabon/man10fishing/contest/FastFishingContest.kt
+++ src/main/java/com/shojabon/man10fishing/contest/FastFishingContest.kt
@@ -1,37 +1,50 @@
 package com.shojabon.man10fishing.contest
 
-import com.shojabon.man10fishing.Man10Fishing
 import com.shojabon.man10fishing.dataClass.FishParameter
 import org.bukkit.Bukkit
 import org.bukkit.boss.BarColor
 import org.bukkit.boss.BarStyle
 import org.bukkit.entity.Player
+import java.util.*
+import kotlin.collections.HashMap
 
 class FastFishingContest:AbstractFishContest() {
 
+
+    //configからとるもの
     private val targetFishList=config.getList("targetFishes")
     private val targetFishName=config.getString("targetFishName","魚")
     private val targetFishAmount=config.getInt("amount")
-
     private var winningPlayerLimit=config.getInt("winnerPlayerLimit", 3)
-
     //rewardCommandsの名前はここだけ変えた方が良いかもしれない
     private var rewardCommands =config.getStringList("rewardCommands")
-    private var winner:Player?=null
 
+
+    private val fishCounter=HashMap<UUID,Int>()
+    private var winner:Player?=null
     private val bossBar = Bukkit.createBossBar("§e§l一番はじめに&c&l${targetFishName}§e§lを§c§l${targetFishAmount}匹§e§l釣れ！", BarColor.BLUE, BarStyle.SOLID)
 
 
     override fun onCaughtFish(player: FishContestPlayer, fish: FishParameter) {
+
         if(targetFishList!=null&&!targetFishList.contains(fish.fish.name))return
+        if(!fishCounter.containsKey(player.uuid))fishCounter[player.uuid]=0
+        fishCounter[player.uuid]=fishCounter[player.uuid]!!+1
+
+        broadCastPlayers("§f${player.name}§aが§e${fishCounter[player.uuid]!!}匹目§aの${targetFishName}を釣り上げた!")
 
         //このstopがonEndを実行するかどうか未確認
-        time.stop()
+        if(fishCounter[player.uuid]!!>=targetFishAmount)time.stop()
     }
 
     override fun onStart() {
-        TODO("Not yet implemented")
+        time.setRemainingTime(config.getInt("time", 60))
+        players.keys.forEach {
+            bossBar.addPlayer(Bukkit.getPlayer(it)?:return@forEach)
+        }
+        time.linkBossBar(bossBar, true)
     }
+
     override fun onEnd() {
 
         broadCastPlayers("§c§lコンテスト終了!!")
