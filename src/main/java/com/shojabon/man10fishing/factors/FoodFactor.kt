9048e4473687736703cd987999c707db46577bcc
JToTl <80580108+JToTl@users.noreply.github.com>
2024-10-27 20:35:50 +0900

    倍率修正

--- src/main/java/com/shojabon/man10fishing/factors/FoodFactor.kt
+++ src/main/java/com/shojabon/man10fishing/factors/FoodFactor.kt
@@ -7,6 +7,7 @@ import com.shojabon.man10fishing.annotations.FishFactorDefinition
 import com.shojabon.man10fishing.dataClass.*
 import org.bukkit.Bukkit
 import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.pow
 
 @FishFactorDefinition(name = "フード",
@@ -26,9 +27,10 @@ class FoodFactor(fish: Fish) : FishFactor(fish) {
         val foodRange=if(rod.currentFood[5]>0)rod.currentFood[5] else 0.0
         val fishRange=range.get()!!
         val poweredDistance=nDimensionDistanceSquared(matrix.get()!!, rod.currentFood.subList(0,5))
+        val rangeDistanceMultiplier=if(foodRange<0.0) 2.5F else max((750.0F/foodRange.toFloat()),1.0F)
 
         return if (poweredDistance <= foodRange.pow(2.0)+fishRange.pow(2.0)+2*foodRange*fishRange) {
-            currentMultiplier * foodInRangeMultiplier
+            currentMultiplier * foodInRangeMultiplier*rangeDistanceMultiplier
         } else currentMultiplier
     }
 
