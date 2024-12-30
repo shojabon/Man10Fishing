e90526db2b4f5f08c79b65f51e0f5c803c72f2d7
JToTl <80580108+JToTl@users.noreply.github.com>
2024-12-31 04:30:09 +0900

    0除算

--- src/main/java/com/shojabon/man10fishing/factors/FoodFactor.kt
+++ src/main/java/com/shojabon/man10fishing/factors/FoodFactor.kt
@@ -8,6 +8,7 @@ import com.shojabon.man10fishing.dataClass.*
 import org.bukkit.Bukkit
 import kotlin.math.abs
 import kotlin.math.max
+import kotlin.math.min
 import kotlin.math.pow
 
 @FishFactorDefinition(name = "フード",
@@ -23,11 +24,10 @@ class FoodFactor(fish: Fish) : FishFactor(fish) {
     override fun rarityMultiplier(fish: Fish, currentMultiplier: Float, fisher: Player, rod: FishingRod): Float {
 
         //food,fishの半径及び中心の距離をとり、円同士が重なっているかどうかを半径と中心の距離との関係で評価
-        // x<y+z == x^2<(y+z)^2を利用
         val foodRange=if(rod.currentFood[5]>0)rod.currentFood[5] else 0.0
         val fishRange=range.get()!!
         val poweredDistance=nDimensionDistanceSquared(matrix.get()!!, rod.currentFood.subList(0,5))
-        val rangeDistanceMultiplier=if(foodRange<0.0) 2.5F else max((750.0F/foodRange.toFloat()),1.0F)
+        val rangeDistanceMultiplier=if(foodRange<0.0) 2.5F else max(min(650.0F/(1+foodRange.toFloat()),6.0F),1.0F)
 
         return if (poweredDistance <= foodRange.pow(2.0)+fishRange.pow(2.0)+2*foodRange*fishRange) {
             currentMultiplier * foodInRangeMultiplier*rangeDistanceMultiplier
